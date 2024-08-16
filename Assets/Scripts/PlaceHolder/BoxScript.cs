e79b8439ab8ed94b7e605491e1f7332a03b5eed9
Felix <jugj@users.noreply.github.com>
2024-08-16 20:51:11 +0200

    Änderungen von Felix um 20:51:11

---
+++ Assets/Scripts/PlaceHolder/BoxScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScript : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        if(col.gameObject.tag == "PlayerAttack") 
+        {
+            particle.Play();
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,27 +7,79 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     Vector2 movementDir;
-    Rigidbody2D rb;
-    float vertical;
-    float horizontal;
+    public Rigidbody2D rb;
+    public Animator anim;
+    bool isFacingRight = true;
+    public float attackTime;
+    bool canAttack = true;
+    float timer;
+    public GameObject attackCollider;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        timer = attackTime;
     }
 
     void Update()
     {
+        GetInput();
+        Flip();
+        Animation();
 
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        movementDir = new Vector2(horizontal, vertical).normalized;
+        if(Input.GetButtonDown("Jump") && canAttack) 
+        {
+            anim.SetTrigger("attack");
+            canAttack = false;
+        }
 
+        if(!canAttack) 
+        {
+            attackCollider.SetActive(true);
+        }
+        else 
+        {
+            attackCollider.SetActive(false);
+        }
+
+        if(!canAttack && timer > 0) 
+        {
+            timer -= Time.deltaTime;
+        }
+        
+        else if(timer < 0 && !canAttack) 
+        {
+            timer = attackTime;
+            canAttack = true;
+        }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(movementDir.x * speed, movementDir.y * speed);
+    }
+
+    void GetInput() 
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDir = new Vector2(horizontal, vertical).normalized;
+    }
+
+    void Animation() 
+    {
+        if(rb.velocity.x != 0 || rb.velocity.y != 0) 
+        {
+            anim.SetFloat("speed", 1);
+        }
+        else 
+        {
+            anim.SetFloat("speed", 0);
+        }
+    }
+
+    void Flip() 
+    {
     }
 
    
