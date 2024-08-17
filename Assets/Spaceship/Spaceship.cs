65b70a27e63348fca40f2b2f819125601b922a90
Paul <jugj@users.noreply.github.com>
2024-08-17 18:06:12 +0200

    Änderungen von Paul um 18:06:12

---
+++ Assets/Spaceship/Spaceship.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class Spaceship : MonoBehaviour
+{
+   public float Geschwindigkeit;
+
+  public int maxmana = 5;
+  public int currentmana;
+
+  public ManaBar manabar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentmana = maxmana;
+        manabar.SetMaxMana(4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w"))
+      transform.Translate(Vector2.up*Geschwindigkeit*Time.deltaTime,Space.World);
+
+        if(Input.GetKey("a"))
+      transform.Translate(Vector2.left*Geschwindigkeit*Time.deltaTime,Space.World);
+
+        if(Input.GetKey("s"))
+      transform.Translate(Vector2.down*Geschwindigkeit*Time.deltaTime,Space.World);
+
+        if(Input.GetKey("d"))
+      transform.Translate(Vector2.right*Geschwindigkeit*Time.deltaTime,Space.World);
+        
+        if(Input.GetKey("x")){
+        currentmana -=1;
+         manabar.SetCurrentMana (currentmana);
+        }
+
+        if(Input.GetKey("r")){
+        currentmana +=1;
+         manabar.SetCurrentMana (currentmana);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        //reduce mana by 1
+        currentmana -=1;
+        manabar.SetCurrentMana (currentmana);
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Sprites UI workshop/ManaBar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ManaBar : MonoBehaviour
+{
+
+public int currentmana;
+public int maxmana = 4;
+    public Slider slider;
+
+    //set max mana
+    public void SetMaxMana(int maxmana){
+        slider.maxValue = maxmana;
+        slider.value = currentmana;
+    }
+    //set current mana
+    public void SetCurrentMana(int currentmana){
+slider.value = currentmana;
+    }
+
+
+}
+
