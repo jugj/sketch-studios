f7915afe7ce6ab733cf8f49fadd87faa67cad624
Felix <jugj@users.noreply.github.com>
2024-08-17 15:42:38 +0200

    Änderungen von Felix um 15:42:38

--- Assets/Scripts/Enemy/AI.cs
+++ Assets/Scripts/Enemy/AI.cs
@@ -5,16 +5,26 @@ public class AI : MonoBehaviour
     public Transform player; 
     public float moveSpeed = 4f; 
     public float detectionRange = 10f;
+    public FieldOfView FoV;
+    public Animator anim;
+    Rigidbody2D rb;
 
     void Update()
     {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(FoV.canSeePlayer) 
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
          if (distanceToPlayer <= detectionRange)
             {
                 Vector2 direction = (player.position - transform.position).normalized;
 
                 transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+
             }
+        }
+        
+        
     }
 }
